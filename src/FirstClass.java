aaaeaa62318fd69b40c8aad605897be3c799bf26
h47assan <h47assan@gmail.com>
2022-07-07 20:37:05 -0400

    2nd commit

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,6 +1,7 @@
 public class FirstClass {
     public static void main(String[] args) {
 
-        System.out.printf("Hello Git User");
+        System.out.println("Hello Git User");
+        System.out.println("I think I did it ! ");
     }
 }
